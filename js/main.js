985c08148a8ae3bd33de04a31a6bc2cadbb49d3b
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-16 19:08:06 +0100

    Tarea: El evento del Botón ID retorna un console.log.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 
 const inputElement = document.querySelector("#inputValue");
 const searchMovie = document.querySelector("#searchMovie");
+const searchID = document.querySelector("#searchID");
 
 // Recursos API
 
@@ -12,20 +13,16 @@ let key = ("bb78e4cf3442e302d928f2c5edcdbee1");
 
 let url = "https://api.themoviedb.org/3/search/movie?api_key=bb78e4cf3442e302d928f2c5edcdbee1"    // SEARCH A MOVIE
 
-let recurso = "search";
 
-let criterio = "movie";
-
-
-// Funciones
+// Funciones:
 
 
-// Funciones para clickar en el boton y obtener su valor:
+// Funcion Botón Search a Movie
 
 searchMovie.onclick = function(event) {
     // .preventDefault: Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del evento, es decir, puede ser llamado de nuevo.
     event.preventDefault();
-    console.log("The Search a Movie Button has been clicked!!!")
+    console.log("The Search a Movie Button has been clicked!!!");
 
     // Obtengo el valor de inputElement
     const value = inputElement.value;
@@ -36,9 +33,17 @@ searchMovie.onclick = function(event) {
     fetch(newUrl)
         .then((res) => res.json())
         .then((data) => {
+            // data.results []
             console.log("Data: ", data)
         })
         .catch((error) => {
             console.log("Error", error)
         });
 };
+
+// Función Botón Search a Movie by ID
+
+searchID.onclick = function(event) {
+    event.preventDefault();
+    console.log("The Search by ID Button has been clicked!!!");
+}
