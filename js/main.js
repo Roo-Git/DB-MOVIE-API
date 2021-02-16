3bb40d8ef35ac9eccc522933300fcfa1f376a826
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-16 02:11:21 +0100

    Tarea: Función Buscar Pelicula realizada.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+// https://api.themoviedb.org/3/search/movie?api_key=bb78e4cf3442e302d928f2c5edcdbee1&query=Mad //
+
+
 // Seleccionar Elementos del DOM
 
 const inputElement = document.querySelector("#inputValue");
 const searchMovie = document.querySelector("#searchMovie");
 
+// Recursos API
+
+let key = ("bb78e4cf3442e302d928f2c5edcdbee1");
+
+let url = "https://api.themoviedb.org/3/search/movie?api_key=bb78e4cf3442e302d928f2c5edcdbee1"    // SEARCH A MOVIE
+
+let recurso = "search";
+
+let criterio = "movie";
 
 
 // Funciones
@@ -17,5 +29,16 @@ searchMovie.onclick = function(event) {
 
     // Obtengo el valor de inputElement
     const value = inputElement.value;
-    console.log("Value: ", value);         
+    console.log("Value: ", value);
+
+    const newUrl = url + "&query=" + value;
+    
+    fetch(newUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("Data: ", data)
+        })
+        .catch((error) => {
+            console.log("Error", error)
+        });
 };
